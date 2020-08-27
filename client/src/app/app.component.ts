a195407961f4656b60ddb324d9999f401af6f930
Nathan Booth <nathan_booth@live.com.au>
2020-08-27 12:53:29 +1000

    end of section 17

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { IProduct } from './shared/models/product';
 import { BasketService } from './basket/basket.service';
+import { AccountService } from './account/account.service';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +13,23 @@ export class AppComponent implements OnInit {
   title = 'SkiNet';
   products: IProduct[];
 
-  constructor(private basketSerivce: BasketService) {  }
+  constructor(private basketSerivce: BasketService, private accountService: AccountService) {  }
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    this.accountService.loadCurrentUser(token).subscribe(() => {
+      console.log('loaded user');
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
 
     if (basketId) {
